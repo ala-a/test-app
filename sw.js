304a178ef75fa951391346f73c197c58ee1bd866
Ala Avoyan <aa3175@nyu.edu>
2025-09-24 14:58:25 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open("pc-v1").then((c) => c.addAll(["/", "/index.html", "/manifest.json", "/icons/icon-192.png", "/icons/icon-512.png"]))); 
-});
-self.addEventListener("fetch", (e) => {
-  const url = new URL(e.request.url);
-  if (url.origin === location.origin) {
-    e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
-  }
-});
